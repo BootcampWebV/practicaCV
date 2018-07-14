fd64698bee4bb5b82840ca20267d748d4120b27d
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-07-14 10:36:20 +0200

    cambios en estructura de contactos ahora se obtienen asincronamente

--- js/main.js
+++ js/main.js
@@ -1,54 +1,9 @@
-//import { FormContact} from './form-contact.js'
 import { Menu } from './menu.js';
+import { FormContact} from './form.js';
 
 export class Main {
     constructor() {
-        // elementos del DOM
-        // skills
-        this.oMenuButtonWhoiam = document.querySelector('#menu-btn-whoiam');         
-        this.oSkillsJava = document.querySelector('#skills-java');
-        this.oSkillsSql = document.querySelector('#skills-sql');
-        this.oSkillsHtml = document.querySelector('#skills-html');
-        this.oSkillsCss = document.querySelector('#skills-css');
-        this.oSkillsJs = document.querySelector('#skills-js'); 
-        //this.oBotonOlderPost = document.querySelector('#olderPostsBtn')
-        //this.oInputName = document.querySelector('#name')
         this.oMenus = new Menu();
-        //this.oFormContact = new FormContact()
-        console.log(this);
-        this.defineEventListeners()
+        this.oFormContact = new FormContact();        
     }
-
-    defineEventListeners() {
-        //this.oBotonOlderPost.onclick = this.verOlderPosts
-        // eventos
-        this.oMenuButtonWhoiam.addEventListener('click', this.toShowSkills.bind(this));    
-        /*this.oBotonOlderPost.addEventListener('click', this.verOlderPosts.bind(this))
-        this.oInputName.addEventListener('change', this.probarInput.bind(this))
-        this.oInputName.addEventListener('input', this.probarInput.bind(this))*/
-    }
-
-    toShowSkills() {
-        console.info('Mostrando habilidades!!!');             
-        this.oSkillsJava.classList.add('skill-java-loading');
-        this.oSkillsSql.classList.add('skill-sql-loading');
-        this.oSkillsHtml.classList.add('skill-html-loading');
-        this.oSkillsCss.classList.add('skill-css-loading');
-        this.oSkillsJs.classList.add('skill-js-loading');
-    }
-
-    /*verOlderPosts(oE) {
-        console.dir(oE)
-    }
-
-    probarInput(oE) {
-        if(oE.type == "change") {
-            console.log('change')
-            console.dir(oE.target.value)
-        } else if (oE.type == "input") {
-            console.log('input')
-            console.dir(oE.target.value)
-        }
-    }*/
 }
-
--- js/menu.js
+++ js/menu.js
@@ -1,81 +1,111 @@
 export class Menu {
     constructor () {
-        // navs
 
-        this.aMenuItems = document.querySelectorAll("nav#menu-top a");
-       
-        this.oBotonMenu1 = document.querySelector('#menu-btn-1')
-        this.oBotonMenu2 = document.querySelector('#menu-btn-2')
-        this.oMenuTop =  document.querySelector('#menu-top')
-        // Otros
-        this.aSections = document.querySelectorAll("section")
-        this.oOffsets = []
+        // nav bar
+        this.aMenuItems = document.querySelectorAll("nav#menu a");
+        this.oButtonMenuOpen = document.querySelector('#menu-btn-open');
+        this.oButtonMenuClose = document.querySelector('#menu-btn-close');        
+        this.oMenu = document.querySelector('#menu');
+        this.oMenuButtonHome = document.querySelector('#menu-btn-home');  
+        this.oMenuButtonWhoiam = document.querySelector('#menu-btn-whoiam');         
+        this.oMenuButtonWorkexperience = document.querySelector('#menu-btn-workexperience'); 
+        this.oMenuButtonAboutme = document.querySelector('#menu-btn-aboutme');  
+        this.oMenuButtonContact = document.querySelector('#menu-btn-contact');       
+        
+        // skills               
+        this.oSkillsJava = document.querySelector('#skills-java');
+        this.oSkillsSql = document.querySelector('#skills-sql');
+        this.oSkillsHtml = document.querySelector('#skills-html');
+        this.oSkillsCss = document.querySelector('#skills-css');
+        this.oSkillsJs = document.querySelector('#skills-js');      
+
+        // Otros        
+        this.aSections = document.querySelectorAll("section");
+        this.oOffsets = [];
+
         // eventos
-        this.oBotonMenu1.addEventListener('click', this.toggleMenu.bind(this));
-        this.oBotonMenu2.addEventListener('click', this.toggleMenu.bind(this));
+        this.oButtonMenuOpen.addEventListener('click', this.toggleMenu.bind(this));
+        this.oButtonMenuClose.addEventListener('click', this.toggleMenu.bind(this));
         this.aMenuItems.forEach(
-            (item) => { item.addEventListener('click', this.activarItem.bind(this));}
+            (item) => { item.addEventListener('click', this.activateItem.bind(this));}
         )
         window.addEventListener('scroll', this.changeMenuStyle.bind(this));
+        
+        this.oMenuButtonHome.addEventListener('click', this.smoothScroll.bind(this));
+        this.oMenuButtonWhoiam.addEventListener('click', this.smoothScroll.bind(this));        
+        this.oMenuButtonWorkexperience.addEventListener('click', this.smoothScroll.bind(this));
+        this.oMenuButtonAboutme.addEventListener('click', this.smoothScroll.bind(this));
+        this.oMenuButtonContact.addEventListener('click', this.smoothScroll.bind(this));
+        
+        /* Para controlar que la activacion del menu se realice solo en el metodo activateItem o en changeMenuStyle, 
+        en los dos no porque se solapan */
+        this.activate = true;
 
-        this.prepararNavegacion();
+        //Para controlar origen en smoth scroll
+        this.origin = '';
+        this.prepareNavigation();
     }
 
     toggleMenu(oE) {
         // elimina la ejecucion del click en el elemento
-        oE.preventDefault()
+        oE.preventDefault();
         // cambia su visibilidad
-        oE.target.classList.toggle('hide')
+        oE.target.classList.toggle('hide');
         // cambia la visibilidad del otro icono
         if (oE.target.previousElementSibling) {
-            oE.target.previousElementSibling.classList.toggle('hide')
+            oE.target.previousElementSibling.classList.toggle('hide');
         } else {
-            oE.target.nextElementSibling.classList.toggle('hide')
+            oE.target.nextElementSibling.classList.toggle('hide');
         }
          // cambia la visibilidad del menu top para mobile
-        this.oMenuTop.classList.toggle('hide')
+        this.oMenu.classList.toggle('hide');
     }
 
     // Se activa el elemento del menu sobre el que hagamos click
-    activarItem(oE) {
-        console.log('Activando Item')
+    activateItem(oE) {
         this.aMenuItems.forEach(
-            (item) => { item.classList.remove('active')}
+            (item) => { item.classList.remove('active');}
         )
-        oE.target.classList.add('active')
+        oE.target.classList.add('active');
+        this.activate = false;
     }
-
 
     // Para poner activo el elemento del menu ha cuya seccion relacionada hayamos llegado a traves del scroll
     changeMenuStyle () {
-        let pageOffset = window.pageYOffset
-        let menuItem = 0
+        let pageOffset = window.pageYOffset;
+        let menuItem = 0;
+        this.toHideSkills();
         if (pageOffset >=  this.oOffsets['#home'] && pageOffset < this.oOffsets['#whoiam']) {
-            menuItem = 0
-        } else if (pageOffset >= this.oOffsets['#whoiam'] && pageOffset < this.oOffsets['#workexperience']) {
-            menuItem = 1
+            menuItem = 0;
+        } else if (pageOffset >=  this.oOffsets['#whoiam'] && pageOffset < this.oOffsets['#workexperience']) {
+            this.toShowSkills();
+            menuItem = 1;
         } else if (pageOffset >= this.oOffsets['#workexperience'] && pageOffset < this.oOffsets['#aboutme']) {
-            menuItem = 2   
+            menuItem = 2;
         } else if (pageOffset >= this.oOffsets['#aboutme'] && pageOffset < this.oOffsets['#contact']) {
-            menuItem = 3    
+            menuItem = 3;
         } else {
-            menuItem = 4
+            menuItem = 4;
         }
-        this.aMenuItems.forEach(
-            (item) => item.classList.remove('active')
-        )
-        this.aMenuItems[menuItem].classList.add('active')
+
+        if ( this.activate === true) {
+            this.aMenuItems.forEach( 
+                (item) => item.classList.remove('active')            
+            )
+            this.aMenuItems[menuItem].classList.add('active');
+        }
+            
+        this.activate = true;
     }
 
-    //
-    prepararNavegacion() {
+    prepareNavigation() {
         this.aSections.forEach(
             (item) => {
                 let cumulative =  this.cumulativeOffset(item);
                 this.oOffsets['#'+item.id] = cumulative;
             }
         )
-    
+        this.origin = '#home';
     }
 
     // Obtiene la posicion de cada seccion de la pagina
@@ -86,5 +116,65 @@ export class Menu {
             element = element.offsetParent;
         } while(element);
         return top;
+    }
+
+    //Para mostrar las habilidades al llegar a la seccion sobre mi
+    toShowSkills() {
+        this.oSkillsJava.classList.add('skill-java-loading');
+        this.oSkillsSql.classList.add('skill-sql-loading');
+        this.oSkillsHtml.classList.add('skill-html-loading');
+        this.oSkillsCss.classList.add('skill-css-loading');
+        this.oSkillsJs.classList.add('skill-js-loading');
+    }
+
+    //Para ocultar las habilidades al llegar a la seccion sobre mi
+    toHideSkills() {
+        this.oSkillsJava.classList.remove('skill-java-loading');
+        this.oSkillsSql.classList.remove('skill-sql-loading');
+        this.oSkillsHtml.classList.remove('skill-html-loading');
+        this.oSkillsCss.classList.remove('skill-css-loading');
+        this.oSkillsJs.classList.remove('skill-js-loading');   
+    }
+
+    smoothScroll(aE) {        
+        let origin = this.oOffsets[this.origin];
+        let destiny = this.oOffsets[aE.target.hash];
+
+        // seteamos el nuevo origen
+        this.origin = aE.target.hash;
+        let distance = destiny > origin ? destiny - origin : origin - destiny;
+        let speed = Math.round(distance / 100);
+        let step = Math.round(distance / 25);
+        let diferenceOriginDestiny = destiny > origin ? origin + step : origin - step;
+        let timer = 0;
+
+        if (distance < 100) {
+            scrollTo(0, destiny); 
+            return;
+        }
+       
+        if (speed >= 20) {
+            speed = 20;
+        }
+             
+        if (destiny > origin) {
+            for ( let i=origin; i<destiny; i+=step ) {
+                setTimeout("window.scrollTo(0, " + diferenceOriginDestiny + ")", timer * speed);
+                diferenceOriginDestiny += step; 
+                if (diferenceOriginDestiny > destiny) {
+                    diferenceOriginDestiny = destiny; 
+                }
+                timer++;
+            } return;
+        }
+
+        for ( let i=origin; i>destiny; i-=step ) {
+            setTimeout("window.scrollTo(0, " + diferenceOriginDestiny + ")", timer * speed);
+            diferenceOriginDestiny -= step; 
+            if (diferenceOriginDestiny < destiny) {
+                diferenceOriginDestiny = destiny; 
+            }
+            timer++;
+        }        
     }
 }
